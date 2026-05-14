233808843850ee1a34143fdf2c023b108e3cd9fc
Hemasekhar Puchuginjala <hemasekhar@google.com>
2026-05-14 15:39:02 +0530

    Adding code to build fail

--- src/main/java/com/genai/test/GreetingResource.java
+++ src/main/java/com/genai/test/GreetingResource.java
@@ -1,5 +1,6 @@
 package com.genai.test;
 
+import com.google.genai.Client;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -11,6 +12,7 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        Client client = new Client();
         return "Hello from Quarkus REST";
     }
 }
